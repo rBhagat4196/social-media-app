80302796d98dff65d4b44b26625bbff72952e049
rBhagat4196 <rahulbhagat13slr@gmail.com>
2024-01-22 21:13:10 +0530

    login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,29 +1,202 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { BsShare } from "react-icons/bs";
+import { AiOutlineInteraction } from "react-icons/ai";
+import { ImConnection } from "react-icons/im";
+import TextInput from "../components/TextInput";
+import Loading from "../components/Loading";
+import BgImage from "../assets/bg-image.webp";
 
 const LoginPage = () => {
-  return (
-    <div className='bg-bgColor w-full h-[100vh] flex items-center justify-center p-5'>
-      <div className='w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary rounded-xl overflow-hidden shadow-xl '>
-      {/* LEFT  */}
-      <div className="w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center">
-        <div className="w-full flex gap-2 items-center mb-6">
-          <div className="p-2 rounded text-white">
-            <img src="/reshot-icon-flickr-PTXY7M2H6V.svg"></img>
-          </div>
-          <span className="text-2xl text-[#065ad8] font-semibold">
-            Share with fun
-          </span>
-        </div>
-        <p className="text-ascent-1 text-base font-semibold">Log in to your account</p>
-        <span className="text-sm mt-2 text-ascent-2">Welcom Back</span>
-        <form className="py-8 flex flex-col gap-5">
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-        </form>
-      </div>
-      {/* RIGHT */}
-      <div></div>
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleInputChange = (name, value) => {
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+
+    setErrors({
+      ...errors,
+      [name]: "",
+    });
+  };
+
+  const onSubmit = async () => {
+    // console.log("Form submitted with data:", formData);
+  
+    // if (!formData.email) {
+    //   setErrors({
+    //     ...errors,
+    //     email: "Email Address is required",
+    //   });
+    // }
+  
+    if (!formData.password && !formData.email) {
+      setErrors({
+        email: "Email Address is required",
+        password: "Password is required",
+      });
+    }
+    else if(!formData.password){
+      setErrors({
+        email: "",
+        password: "Password is required",
+      });
+    }
+    else if(!formData.email){
+      setErrors({
+        email: "Email is required",
+        password: "",
+      });
+    }
+
+    // console.log(errors)
+    setIsSubmitting(true);
+  
+      setTimeout(() => {
+      setIsSubmitting(false);
+  
+      if (formData.email === "example@example.com" && formData.password === "password") {
+        // Successful login
+        console.log("Login successful");
+      } else {
+        // Failed login
+        console.log("Invalid login credentials");
+      }
+    }, 2000);
+  };
+  
+
+  return (
+    <div className='bg-bgColor w-full h-[100vh] flex items-center justify-center p-6'>
+      <div className='w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary rounded-xl overflow-hidden shadow-xl'>
+        {/* LEFT */}
+        <div className='w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center'>
+          <div className='w-full flex gap-2 items-center mb-6'>
+            <div className='p-2 rounded text-white'>
+              <img src="/reshot-icon-flickr-PTXY7M2H6V.svg" className="w-[50px]"></img>
+            </div>
+            <span className='text-2xl text-[#065ad8] font-semibold'>
+              ShareFun
+            </span>
+          </div>
+
+          <p className='text-ascent-1 text-base font-semibold'>
+            Log in to your account
+          </p>
+          <span className='text-sm mt-2 text-ascent-2'>Welcome back</span>
+
+          <form
+            className='py-8 flex flex-col gap-5'
+            onSubmit={(e) => {
+              e.preventDefault();
+              onSubmit();
+            }}
+          >
+            {/* Email TextInput */}
+            <div className='w-full flex flex-col mt-2'>
+              <TextInput
+                type='email'
+                placeholder='email@example.com'
+                styles='w-full rounded-full bg-secondary border border-[#66666690] outline-none text-sm text-ascent-1 px-4 py-3 placeholder:text-[#666]'
+                value={formData.email}
+                onChange={(e) => handleInputChange("email", e.target.value)}
+                error={errors.email}
+              />
+            </div>
+
+            {/* Password TextInput */}
+            <div className='w-full flex flex-col mt-2'>
+              <TextInput
+                type='password'
+                placeholder='Password'
+                styles='w-full rounded-full bg-secondary border border-[#66666690] outline-none text-sm text-ascent-1 px-4 py-3 placeholder:text-[#666]'
+                value={formData.password}
+                onChange={(e) => handleInputChange("password", e.target.value)}
+                error={errors.password}
+              />
+            </div>
+
+            {/* Forgot Password Link */}
+            <Link
+              to='/reset-password'
+              className='text-sm text-right text-blue font-semibold ml-auto px-2 w-[200px]'
+            >
+              Forgot Password?
+            </Link>
+
+            {/* Submit Button */}
+            {isSubmitting ? (
+              <Loading />
+            ) : (
+              <button
+                type='submit'
+                className='mx-2 inline-flex justify-center rounded-md bg-blue px-8 py-3 text-sm font-medium text-white outline-none'
+              >
+                Login
+              </button>
+            )}
+          </form>
+
+          {/* Create Account Link */}
+          <p className='text-ascent-2 text-sm text-center'>
+            Don't have an account?
+            <Link
+              to='/register'
+              className='text-[#065ad8] font-semibold ml-2 cursor-pointer'
+            >
+              Create Account
+            </Link>
+          </p>
+        </div>
+        {/* RIGHT */}
+        <div className='hidden w-1/2 h-full lg:flex flex-col items-center justify-center bg-blue'>
+          <div className='relative w-full flex items-center justify-center'>
+            <img
+              src={BgImage}
+              alt='Bg Image'
+              className='w-48 2xl:w-64 h-48 2xl:h-64 rounded-full object-cover'
+            />
+
+            <div className='absolute flex items-center gap-1 bg-white right-10 top-10 py-2 px-5 rounded-full'>
+              <BsShare size={14} />
+              <span className='text-xs font-medium'>Share</span>
+            </div>
+
+            <div className='absolute flex items-center gap-1 bg-white left-10 top-6 py-2 px-5 rounded-full'>
+              <ImConnection />
+              <span className='text-xs font-medium'>Connect</span>
+            </div>
+
+            <div className='absolute flex items-center gap-1 bg-white left-12 bottom-6 py-2 px-5 rounded-full'>
+              <AiOutlineInteraction />
+              <span className='text-xs font-medium'>Interact</span>
+            </div>
+          </div>
+
+          <div className='mt-16 text-center'>
+            <p className='text-white text-base'>
+              Connect with friends & have fun sharing
+            </p>
+            <span className='text-sm text-white/80'>
+              Share memories with friends and the world.
+            </span>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
