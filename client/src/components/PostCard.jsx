447f0f4afbd0f4db443cf7b24a2f4f4fcf8ec514
rBhagat4196 <rahulbhagat13slr@gmail.com>
2024-01-24 19:35:31 +0530

    conditional description show-casing + post image

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import NoProfile from '../assets/userprofile.png'
 import { Link } from "react-router-dom";
+import moment from "moment";
 const PostCard = ({post , user , deletePost , likePost}) => {
     const [showAll , setShowAll] = useState(0);
     const [showReply , setShowReply] = useState(0);
@@ -13,23 +14,62 @@ const PostCard = ({post , user , deletePost , likePost}) => {
 
   return (
     <div className="mb-2 bg-primary p-4 rounded-xl">
-        <Link to={"/profile/"+post?.userId?._id}>
-      <div className="flex gap-3 items-center mb-2">
-        <img
-              src={post?.userId?.profileUrl ?? NoProfile}
-              alt={post?.userId?.firstName}
-              className='w-14 h-14 object-cover rounded-full'
-            />
-        <div className='flex flex-col justify-center'>
-              <p className='text-lg font-medium text-ascent-1'>
+      <div className='flex gap-3 items-center mb-2'>
+        <Link to={"/profile/" + post?.userId?._id}>
+          <img
+            src={post?.userId?.profileUrl ?? NoProfile}
+            alt={post?.userId?.firstName}
+            className='w-14 h-14 object-cover rounded-full'
+          />
+        </Link>
+
+        <div className='w-full flex justify-between'>
+          <div className=''>
+            <Link to={"/profile/" + post?.userId?._id}>
+              <p className='font-medium text-lg text-ascent-1'>
                 {post?.userId?.firstName} {post?.userId?.lastName}
               </p>
-              <span className='text-ascent-2'>
-                {post?.userId?.location}
-              </span>
-            </div>
+            </Link>
+            <span className='text-ascent-2'>{post?.userId?.location}</span>
+          </div>
+
+          <span className='text-ascent-2'>
+            {moment(post?.createdAt ?? "2023-05-25").fromNow()}
+          </span>
+        </div>
       </div>
-        </Link>
+      <div>
+        <p className='text-ascent-2'>
+          {showAll === post?._id
+            ? post?.description
+            : post?.description.slice(0, 300)}
+
+          {post?.description?.length > 301 &&
+            (showAll === post?._id ? (
+              <span
+                className='text-blue ml-2 font-mediu cursor-pointer'
+                onClick={() => setShowAll(0)}
+              >
+                Show Less
+              </span>
+            ) : (
+              <span
+                className='text-blue ml-2 font-medium cursor-pointer'
+                onClick={() => setShowAll(post?._id)}
+              >
+                Show More
+              </span>
+            ))}
+        </p>
+
+        {post?.image && (
+          <img
+            src={post?.image}
+            alt='post image'
+            className='w-full mt-2 rounded-lg'
+          />
+        )}
+      </div>
     </div>
   )
 }
