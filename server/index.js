42de70265838b976b3f80c312fbeafa9245fd075
rBhagat4196 <rahulbhagat13slr@gmail.com>
2024-01-25 23:27:55 +0530

    server started

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import path from "path";
+import helmet from "helmet"; // security package
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middlewares
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
