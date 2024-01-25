3b93a66e4cfcbb37a2f4be3fcbcade877e8ae357
rBhagat4196 <rahulbhagat13slr@gmail.com>
2024-01-26 00:24:38 +0530

    hash function , compare password , json web token function created

---
+++ server/utils/index.js
@@ -0,0 +1,23 @@
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+
+// password hashing 
+export const hashString = async (useValue) => {
+  const salt = await bcrypt.genSalt(10);
+
+  const hashedpassword = await bcrypt.hash(useValue, salt);
+  return hashedpassword;
+};
+
+// compare hashedPassword
+export const compareString = async (userPassword, password) => {
+  const isMatch = await bcrypt.compare(userPassword, password);
+  return isMatch;
+};
+
+// create json webToken
+export function createJWT(id) {
+  return JWT.sign({ userId: id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+}
