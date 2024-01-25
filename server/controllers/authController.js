91f69e73f3bf4d66df569d752e84574e8949a907
rBhagat4196 <rahulbhagat13slr@gmail.com>
2024-01-26 01:12:59 +0530

    login controller created

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,35 +1,89 @@
 import Users from "../models/userModel";
 import { hashString } from "../utils";
+import { sendVerificationEmail } from "../utils/sendEmail";
 
-export const register = async(req,res,next)=>{
-    const {firstName , lastName , email , password } = req.body
+export const register = async (req, res, next) => {
+  const { firstName, lastName, email, password } = req.body;
 
-    // validate field
-    if(!(firstName || lastName || email || password)){
-        next("Provide Required Fields");  // passes argument through next
-        return;
+  // validate field
+  if (!(firstName || lastName || email || password)) {
+    next("Provide Required Fields"); // passes argument through next
+    return;
+  }
+
+  try {
+    const userExist = await Users.findOne({ email });
+
+    if (userExist) {
+      next("Email Address Already Exist");
+      return;
+    }
+    const hashedPassword = await hashString(password);
+    const user = await Users.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    sendVerificationEmail(user, res);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// login functionality
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    //validation
+    if (!email || !password) {
+      next("Please Provide User Credentials");
+      return;
     }
 
-    try{
-        const userExist = await Users.findOne({email});
+    // find user by email
+    const user = await Users.findOne({ email });
+    //   .select("+password").
+    populate({
+      path: "friends",
+      select: "firstName lastName location profileUrl -password",
+    });
 
-        if(userExist){
-            next("Email Address Already Exist");
-            return;
-        }
-        const hashedPassword = await hashString(password);
-        const User = await Users.create({
-            firstName,
-            lastName,
-            email,
-            password : hashedPassword
-        })
-
-        
-    }
-    catch(error){
-        console.log(error)
-        res.status(404).json({message : error.message})
+    if (!user) {
+      next("Invalid email or password");
+      return;
     }
 
-}
+    if (!user?.verified) {
+      next(
+        "User email is not verified. Check your email account and verify your email"
+      );
+      return;
+    }
+
+    // compare password
+    const isMatch = await compareString(password, user?.password);
+
+    if (!isMatch) {
+      next("Invalid email or password");
+      return;
+    }
+
+    user.password = undefined;
+
+    const token = createJWT(user?._id);
+
+    res.status(201).json({
+      success: true,
+      message: "Login successfully",
+      user,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
+  }
+};
