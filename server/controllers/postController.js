30d8d64c1eedc3d209fe00110043a885d5fdbfdc
rBhagat4196 <rahulbhagat13slr@gmail.com>
2024-01-27 13:31:34 +0530

    comment/reply , delete post

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -204,3 +204,141 @@ export const likePost = async (req, res, next) => {
       res.status(404).json({ message: error.message });
     }
   };
+
+  export const likePostComment = async (req, res, next) => {
+    const { userId } = req.body.user;
+    const { id, rid } = req.params;
+  
+    try {
+      if (rid === undefined || rid === null || rid === `false`) {
+        const comment = await Comments.findById(id);
+  
+        const index = comment.likes.findIndex((el) => el === String(userId));
+  
+        if (index === -1) {
+          comment.likes.push(userId);
+        } else {
+          comment.likes = comment.likes.filter((i) => i !== String(userId));
+        }
+  
+        const updated = await Comments.findByIdAndUpdate(id, comment, {
+          new: true,
+        });
+  
+        res.status(201).json(updated);
+      } else {
+        const replyComments = await Comments.findOne(
+          { _id: id },
+          {
+            replies: {
+              $elemMatch: {
+                _id: rid,
+              },
+            },
+          }
+        );
+  
+        const index = replyComments?.replies[0]?.likes.findIndex(
+          (i) => i === String(userId)
+        );
+  
+        if (index === -1) {
+          replyComments.replies[0].likes.push(userId);
+        } else {
+          replyComments.replies[0].likes = replyComments.replies[0]?.likes.filter(
+            (i) => i !== String(userId)
+          );
+        }
+  
+        const query = { _id: id, "replies._id": rid };
+  
+        const updated = {
+          $set: {
+            "replies.$.likes": replyComments.replies[0].likes,
+          },
+        };
+  
+        const result = await Comments.updateOne(query, updated, { new: true });
+  
+        res.status(201).json(result);
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({ message: error.message });
+    }
+  };
+
+  export const commentPost = async (req, res, next) => {
+    try {
+      const { comment, from } = req.body;
+      const { userId } = req.body.user;
+      const { id } = req.params;
+  
+      if (comment === null) {
+        return res.status(404).json({ message: "Comment is required." });
+      }
+  
+      const newComment = new Comments({ comment, from, userId, postId: id });
+  
+      await newComment.save();
+  
+      //updating the post with the comments id
+      const post = await Posts.findById(id);
+  
+      post.comments.push(newComment._id);
+  
+      const updatedPost = await Posts.findByIdAndUpdate(id, post, {
+        new: true,
+      });
+  
+      res.status(201).json(newComment);
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({ message: error.message });
+    }
+  };
+  
+  export const replyPostComment = async (req, res, next) => {
+    const { userId } = req.body.user;
+    const { comment, replyAt, from } = req.body;
+    const { id } = req.params;
+  
+    if (comment === null) {
+      return res.status(404).json({ message: "Comment is required." });
+    }
+  
+    try {
+      const commentInfo = await Comments.findById(id);
+  
+      commentInfo.replies.push({
+        comment,
+        replyAt,
+        from,
+        userId,
+        created_At: Date.now(),
+      });
+  
+      commentInfo.save();
+  
+      res.status(200).json(commentInfo);
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({ message: error.message });
+    }
+  };
+  
+  export const deletePost = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+  
+      await Posts.findByIdAndDelete(id);
+  
+      res.status(200).json({
+        success: true,
+        message: "Deleted successfully",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({ message: error.message });
+    }
+  };
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import userAuth from "../middleware/authMiddleware.js"
-import { createPost ,getPosts ,getPost , getUserPost , getComments, likePost} from "../controllers/postController.js";
+import { createPost ,getPosts ,getPost , getUserPost , getComments, likePost, commentPost, replyPostComment, deletePost} from "../controllers/postController.js";
 
 const router = express.Router()
 
@@ -12,14 +12,15 @@ router.post("/",userAuth,getPosts);
 router.post("/post/:id",userAuth, getPost);
 router.post("/get-user-post/:id",userAuth, getUserPost);
 
-// comments
+// get comments
 router.post("/comments/:postId",getComments)
-
-
-
 // likes 
-
 router.post("like/:id",userAuth,likePost);
+router.post("/like-comment/:id/:rid?",userAuth,likePostComment)
+// post comments / reply
+router.post("comment/:id",userAuth,commentPost);
+router.post("reply-comment/:id",userAuth,replyPostComment);
 
-
+// delete post
+router.delete("/:id",userAuth,deletePost)
 export default router
